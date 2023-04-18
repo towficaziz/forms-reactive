e734a826857166495cb782c4f713f9b2b3a2cf0b
towficaziz <tapolly01@gmail.com>
2023-04-18 02:26:44 +0200

    Reactive: Creating Custom Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,15 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
+
+  constructor(private formBuilder: FormBuilder){}
+
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'userData' : new FormGroup({'username': new FormControl(null, Validators.required),
+      'userData' : new FormGroup({
+      'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
       'email': new FormControl(null, [Validators.required, Validators.email])
     }),
       'gender': new FormControl('male'),
@@ -29,5 +34,12 @@ export class AppComponent implements OnInit{
   onAddHobby(){
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string] : boolean}{
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
