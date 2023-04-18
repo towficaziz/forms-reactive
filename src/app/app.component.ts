17e20e8ba0dffa6d06bb98f432b58c0e88850393
towficaziz <tapolly01@gmail.com>
2023-04-18 23:11:22 +0200

    Reactive: Setting and patching values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,9 +24,29 @@ export class AppComponent implements OnInit{
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna'
+      }
+    })
   }
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   get controls() {
